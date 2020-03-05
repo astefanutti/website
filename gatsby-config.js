eccffb4d3708b7d3e733f8a0aeb11706fc3f2726
Antonin Stefanutti <antonin@stefanutti.fr>
2020-03-05 14:33:56 +0100

    Use a nice SVG favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -98,7 +98,7 @@ module.exports = {
         background_color: '#ffffff',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'static/logo.png',
+        icon: 'static/logo.svg',
       },
     },
     {
