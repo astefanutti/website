50de6042aa04a8c5e19b9c0c0555f781b085c1d5
Antonin Stefanutti <antonin@stefanutti.fr>
2021-01-05 13:00:19 +0100

    Add the gatsby-plugin-catch-links plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -158,6 +158,7 @@ module.exports = {
         ],
       },
     },
+    'gatsby-plugin-catch-links',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sharp',
     'gatsby-plugin-styled-components',
