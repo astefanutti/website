1f04dbd84e7313f9c1551e9d087e3bff6840c716
Antonin Stefanutti <antonin@stefanutti.fr>
2021-01-07 17:32:23 +0100

    Fix dark mode toggle initial state change

--- src/components/dark.tsx
+++ src/components/dark.tsx
@@ -1,22 +1,22 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import useDarkMode from 'use-dark-mode'
 
 export default function () {
-  const darkMode = useDarkMode(false)
+  const darkMode = useDarkMode()
+  const [isClient, setIsClient] = useState(false)
 
   useEffect(() => {
     document.body.style.transition = 'background-color 0.3s ease'
+    setIsClient(true)
   })
 
-  return (
+  // See https://github.com/donavon/use-dark-mode/issues/44
+  return !isClient ? (
+    <div style={{ height: '36px', width: '36px' }}></div>
+  ) : (
     <label className="dayNight">
-      <input
-        type="checkbox"
-        checked={darkMode.value}
-        onInput={darkMode.toggle}
-        onChange={() => void 0}
-      />
+      <input type="checkbox" checked={darkMode.value} onChange={darkMode.toggle} />
       <div />
     </label>
   )
