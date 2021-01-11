895b42f33f79f8917c65954e8bc31e379e3d944b
Antonin Stefanutti <antonin@stefanutti.fr>
2021-01-11 15:13:54 +0100

    Gatsby requires import statements on top in gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,8 @@
-require('katex/dist/katex.min.css');
-
 import React from 'react';
 
 import App from './src/components/App';
+
+require('katex/dist/katex.min.css');
 
 export const wrapRootElement = ({ element }) => {
   return <App>{element}</App>;
