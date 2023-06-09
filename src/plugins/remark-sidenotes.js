96ae80032187338a12dff422ad6bf9ba5c786e34
Antonin Stefanutti <antonin@stefanutti.fr>
2023-06-09 16:13:16 +0200

    Use static sidenote counter

--- src/plugins/remark-sidenotes.js
+++ src/plugins/remark-sidenotes.js
@@ -7,9 +7,10 @@ function sidenotes() {
   return transformer;
 }
 
-const getReplacement = (notesAst, isMarginNote) => {
+const getReplacement = (notesAst, isMarginNote, id) => {
   const span = {
     type: 'span',
+    note: true,
     children: notesAst.children,
     data: {
       hName: 'span',
@@ -24,16 +25,22 @@ const getReplacement = (notesAst, isMarginNote) => {
       span,
     ];
   } else {
-    return [
-      {
-        type: 'span',
-        data: {
-          hName: 'span',
-          hProperties: {
-            className: 'sidenote-number',
-          },
+    const number = {
+      type: 'span',
+      children: [{
+        type: 'text',
+        value: id,
+      }],
+      data: {
+        hName: 'span',
+        hProperties: {
+          className: 'sidenote-number',
         },
       },
+    };
+    span.children.splice(0, 0, number);
+    return [
+      number,
       span,
     ];
   }
@@ -47,20 +54,29 @@ function transformer(tree) {
     const target = select(
       `footnoteDefinition[identifier=${node.identifier}]`,
       tree,
-    )
+    );
 
-    if (!target) throw new Error('No corresponding note found')
+    if (!target) throw new Error('No corresponding note found');
 
     const notesAst = target.children.length && target.children.type === 'paragraph'
       ? target.children.children[0]
-      : target.children[0]
+      : target.children[0];
 
     const isMarginNote = notesAst.children[0].value == MARGINNOTE_SYMBOL;
     if (isMarginNote) {
       notesAst.children = notesAst.children.slice(1);
     }
 
-    parent.children.splice(index, 1, ...getReplacement(notesAst, isMarginNote));
+    // parent.children.splice(index, 1, ...getReplacement(notesAst, isMarginNote));
+    const replacement = getReplacement(notesAst, isMarginNote, node.identifier);
+    const i = parent.children.findIndex(c => c.type !== "span" || !c.note);
+    if (replacement.length === 2) {
+      parent.children.splice(index, 1, replacement[0]);
+      parent.children.splice(i, 0, replacement[1]);
+    } else {
+      parent.children.splice(index, 1);
+      parent.children.splice(i, 0, replacement[0]);
+    }
   })
 
   visit(tree, 'footnoteDefinition', (node, index, parent) => {
